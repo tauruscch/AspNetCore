7a5baec2344c0568dd74f3b91d4e9eb53b8429cb
Hao Kung <HaoK@users.noreply.github.com>
2019-07-05 12:11:43 -0700

    Update Email.cshtml.cs

--- src/Identity/UI/src/Areas/Identity/Pages/V3/Account/Manage/Email.cshtml.cs
+++ src/Identity/UI/src/Areas/Identity/Pages/V3/Account/Manage/Email.cshtml.cs
@@ -134,11 +134,6 @@ namespace Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Manage.Internal
             {
                 var userId = await _userManager.GetUserIdAsync(user);
                 var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
-                while (code.Contains('+'))
-                {
-                    code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
-                }
-
                 var callbackUrl = Url.Page(
                     "/Account/ConfirmEmailChange",
                     pageHandler: null,
@@ -173,10 +168,6 @@ namespace Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Manage.Internal
             var userId = await _userManager.GetUserIdAsync(user);
             var email = await _userManager.GetEmailAsync(user);
             var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            while (code.Contains('+'))
-            {
-                code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            }
             var callbackUrl = Url.Page(
                 "/Account/ConfirmEmail",
                 pageHandler: null,
